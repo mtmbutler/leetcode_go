9e7d696600dfbaf8553461e2b222efd0b3e8c094
Miles Butler <mtmbutler@icloud.com>
2019-09-24 20:33:19 -0700

    Finish solution, add edge cases to tests

--- unique_morse_code_words/unique_morse_code_words.go
+++ unique_morse_code_words/unique_morse_code_words.go
@@ -1,7 +1,53 @@
+// https://leetcode.com/problems/unique-morse-code-words/
 package uniquemorsecodewords
 
-// import "fmt"
+import "strings"
 
+var morse = map[rune]string{
+	'a': ".-",
+	'b': "-...",
+	'c': "-.-.",
+	'd': "-..",
+	'e': ".",
+	'f': "..-.",
+	'g': "--.",
+	'h': "....",
+	'i': "..",
+	'j': ".---",
+	'k': "-.-",
+	'l': ".-..",
+	'm': "--",
+	'n': "-.",
+	'o': "---",
+	'p': ".--.",
+	'q': "--.-",
+	'r': ".-.",
+	's': "...",
+	't': "-",
+	'u': "..-",
+	'v': "...-",
+	'w': ".--",
+	'x': "-..-",
+	'y': "-.--",
+	'z': "--..",
+}
+
+// uniqueMorseRepresentations determines the morse representations of the words passed,
+// then counts the number of unique representations in the resulting list.
 func uniqueMorseRepresentations(words []string) int {
-	return 2
+	// Instantiate an empty set to add representations to
+	set := map[string]struct{}{}
+
+	for _, word := range words {
+		var strBuilder strings.Builder
+		// 1. Convert the word to morse code
+		for _, c := range word {
+			strBuilder.WriteString(morse[c])
+		}
+
+		// 2. Add the transformed word to the set
+		morseRep := strBuilder.String()
+		set[morseRep] = struct{}{}
+	}
+	return len(set)
 }
