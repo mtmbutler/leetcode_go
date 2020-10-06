d379352452b1992090193cb070010087384bc26a
Miles Butler <mbutler@energeia-usa.com>
2020-10-06 09:34:43 -0700

    Minor cleanup

--- subrectangle_queries/subrectangle_queries.go
+++ subrectangle_queries/subrectangle_queries.go
@@ -3,14 +3,12 @@ package subrectanglequeries
 
 // SubrectangleQueries stores a matrix and allows some operations.
 type SubrectangleQueries struct {
-	arr [][]int
+	rectangle [][]int
 }
 
 // Constructor creates a new SubrectangleQueries struct
 func Constructor(rectangle [][]int) SubrectangleQueries {
-	sq := SubrectangleQueries{}
-	sq.arr = rectangle
-	return sq
+	return SubrectangleQueries{rectangle: rectangle}
 }
 
 // UpdateSubrectangle applies newValue to every cell in the given matrix between
@@ -20,14 +18,14 @@ func (this *SubrectangleQueries) UpdateSubrectangle(
 ) {
 	for r := row1; r <= row2; r++ {
 		for c := col1; c <= col2; c++ {
-			this.arr[r][c] = newValue
+			this.rectangle[r][c] = newValue
 		}
 	}
 }
 
 // GetValue retrieves the cell at (row, col)
 func (this *SubrectangleQueries) GetValue(row int, col int) int {
-	return this.arr[row][col]
+	return this.rectangle[row][col]
 }
 
 /**
